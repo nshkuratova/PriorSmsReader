aed59b6ce60e299732535d345b75917c0bcdde2e
Nika Shkuratava <nika_shkuratava@epam.com>
2017-12-12 23:24:52 +0300

    initial

--- app/src/main/java/com/example/nikashkuratova/smsreader/MainActivity.java
+++ app/src/main/java/com/example/nikashkuratova/smsreader/MainActivity.java
@@ -8,17 +8,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback{
 
-    TextView nika;
+    private static final int PERMISSION_READ_SMS= 0;
+    private static final int REQUEST_SMS = 1;
+    private static String[] PERMISSIONS_SMS = {Manifest.permission.READ_SMS};
+    private TextView nika;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,33 +45,25 @@ public class MainActivity extends AppCompatActivity {
         /*if(ContextCompat.checkSelfPermission(getBaseContext(), "android.permission.READ_SMS")
                 == PackageManager.PERMISSION_GRANTED) {}*/
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_SMS)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
+            // Contacts permissions have not been granted.
+            Log.i("Info", "Contact permissions has NOT been granted. Requesting permissions.");
+            requestSMSPermissions();
+            //// TODO: 12.12.2017 check if permissions have been granted
+        } else {
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_SMS)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-//                ActivityCompat.requestPermissions(this,
-//                        new String[]{Manifest.permission.READ_CONTACTS},
-//                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
+            // Contact permissions have been granted. Show the contacts fragment.
+            Log.i("Info",
+                    "Contact permissions have already been granted. Displaying contact details.");
+            showSMS();
         }
 
 
+    }
+
+    private void showSMS(){
         Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"),  new String[] { "_id", "thread_id", "address", "person", "date", "body" }, null, null, null);
 
         if (cursor.moveToFirst()) { // must check the result to prevent exception
@@ -84,6 +81,37 @@ public class MainActivity extends AppCompatActivity {
             // empty box, no SMS
         }
     }
+
+    private void requestSMSPermissions() {
+        // BEGIN_INCLUDE(contacts_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_SMS)) {
+
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example, if the request has been denied previously.
+            Log.i("Info",
+                    "Displaying contacts permission rationale to provide additional context.");
+
+            // Display a SnackBar with an explanation and a button to trigger the request.
+            Snackbar.make(mLayout, "Contacts permissions are needed to demonstrate access",
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Ok", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat
+                                    .requestPermissions(MainActivity.this, PERMISSIONS_SMS,
+                                            REQUEST_SMS);
+                        }
+                    })
+                    .show();
+        } else {
+            // Contact permissions have not been granted yet. Request them directly.
+            ActivityCompat.requestPermissions(this, PERMISSIONS_SMS, REQUEST_SMS);
+        }
+        // END_INCLUDE(contacts_permission_request)
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
