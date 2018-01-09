3776c6fbac1eae8841977620ef5a6a7a96901c5b
nshkuratova <nika.shkuratova@gmail.com>
2018-01-09 17:39:13 +0300

    [WIP] Swipe to refresh is in progress of implementation

--- app/src/main/java/com/example/nikashkuratova/smsreader/MainActivity.java
+++ app/src/main/java/com/example/nikashkuratova/smsreader/MainActivity.java
@@ -7,8 +7,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity
     private static final int MY_PERMISSIONS_REQUEST_READ_SMS = 0;
     private static String[] PERMISSIONS_SMS = {Manifest.permission.READ_SMS};
     ListView listView;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,18 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        // This method performs the actual data-refresh operation.
+                        // The method calls setRefreshing(false) when it's finished.
+                        showSMS();
+                    }
+                }
+        );
 
     }
 
@@ -121,3 +136,5 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 }
+
+
