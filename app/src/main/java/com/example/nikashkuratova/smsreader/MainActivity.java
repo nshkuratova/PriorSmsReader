b4e8e3205e3397c6f0fb40be78977a7d3b4d2155
nshkuratova <nika.shkuratova@gmail.com>
2018-01-16 13:01:20 +0300

    Edit category functionality

--- app/src/main/java/com/example/nikashkuratova/smsreader/MainActivity.java
+++ app/src/main/java/com/example/nikashkuratova/smsreader/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        SharedPrefHelper.saveToSharedPref(smsCategory, this);
     }
 
     @Override
